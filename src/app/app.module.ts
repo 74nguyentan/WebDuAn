08f7a4d969719f6ff03bb06b6c814a5acacfe3f5
74nguyentan <exfick@gmail.com>
2020-07-13 15:33:48 +0700

    NVT add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './Component/header-component/header-component.component';
+import { FooterComponentComponent } from './Component/footer-component/footer-component.component';
+import { LoginComponentComponent } from './Components/login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
