4991c3a8c274d5a3aa8610e08c11237810ceda50
leviethung123 <levan999mt@gmail.com>
2020-07-14 15:38:04 +0700

    Bug Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponentComponent } from './Component/header-component/header-component.component';
-import { FooterComponentComponent } from './Component/footer-component/footer-component.component';
 import { LoginComponentComponent } from './Components/login-component/login-component.component';
 import { HeaderComponent } from './Layouts/header/header.component';
 import { FooterComponent } from './Layouts/footer/footer.component';
-import { ListProductComponent } from './component/list-product/list-product.component';
-import { ListProductsComponent } from './Component/list-products/list-products.component';
-import { ListproductsComponent } from './Components/listproducts/listproducts.component';
+import { ListproductsComponent } from './Components/listProducts/listproducts.component';
+import { NavComponent } from './Layouts/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent,
-    FooterComponentComponent,
     LoginComponentComponent,
     HeaderComponent,
     FooterComponent,
-    ListProductComponent,
-    ListProductsComponent,
-    ListproductsComponent
+    ListproductsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
