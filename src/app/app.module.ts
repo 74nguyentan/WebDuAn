c4904f8acde785b016896a718f916052a72e961d
Duong <dp668906@gmail.com>
2020-07-16 20:47:39 +0700

    update footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HeaderComponent } from './Layouts/header/header.component';
 import { FooterComponent } from './Layouts/footer/footer.component';
 import { ListproductsComponent } from './Components/listProducts/listproducts.component';
 import { NavComponent } from './Layouts/nav/nav.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { LoginComponentComponent } from './Components/login-component/login-component.component';
 
 
 library.add(faCoffee);
 @NgModule({
   declarations: [
     AppComponent,
-     HeaderComponent,
     FooterComponent,
     ListproductsComponent,
-    NavComponent
+    NavComponent,
+    LoginComponentComponent
 
   ],
   imports: [BrowserModule, AppRoutingModule,FontAwesomeModule],
