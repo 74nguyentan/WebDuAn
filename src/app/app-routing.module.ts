aa7faf71a5a49dbd733ae03c311a8955c1370154
74nguyentan <exfick@gmail.com>
2020-07-17 08:47:11 +0700

    edit nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
+import { LoginComponentComponent } from './Components/login-component/login-component.component';
+import { ListproductsComponent } from './Components/listProducts/listproducts.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'listNew',
+    component: ListproductsComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './Components/login-component/login-component.component';
-import { HeaderComponent } from './Layouts/header/header.component';
 import { FooterComponent } from './Layouts/footer/footer.component';
 import { ListproductsComponent } from './Components/listProducts/listproducts.component';
 import { NavComponent } from './Layouts/nav/nav.component';
-
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { FilterComponent } from './Layouts/filter/filter.component';
+import { AdvertisementComponent } from './Layouts/advertisement/advertisement.component';
+// import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+// import { TooltipModule } from 'ngx-bootstrap/tooltip';
+// import { ModalModule } from 'ngx-bootstrap/modal';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponentComponent,
-    HeaderComponent,
     FooterComponent,
     ListproductsComponent,
-    NavComponent
+    NavComponent,
+    HomePageComponent,
+    FilterComponent,
+    AdvertisementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // BsDropdownModule.forRoot(),
+    // TooltipModule.forRoot(),
+    // ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
