6157306e7cb0d7f75f714014e437b5a25759c41c
74nguyentan <exfick@gmail.com>
2020-09-28 14:53:12 +0700

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ProductNewsComponent } from './Components/product-news/product-news.component';
+import { ProductDetailComponent } from './Components/product-detail/product-detail.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,17 +13,10 @@ import { NavComponent } from './Layouts/nav/nav.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
 import { FilterComponent } from './Layouts/filter/filter.component';
 import { AdvertisementComponent } from './Layouts/advertisement/advertisement.component';
-
+import { NgxImageZoomModule } from 'ngx-image-zoom';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ProductNewsComponent } from './Components/product-news/product-news.component';
-//import { library } from '@fortawesome/fontawesome-svg-core';
-//import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
- //import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
- //import { TooltipModule } from 'ngx-bootstrap/tooltip';
-/// import { ModalModule } from 'ngx-bootstrap/modal';
-//library.add(faCoffee);
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,6 +26,7 @@ import { ProductNewsComponent } from './Components/product-news/product-news.com
     NavComponent,
     HomePageComponent,
     FilterComponent,
+    ProductDetailComponent,
     AdvertisementComponent,
     ProductNewsComponent
 
@@ -39,10 +35,8 @@ import { ProductNewsComponent } from './Components/product-news/product-news.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // BsDropdownModule.forRoot(),
-    // TooltipModule.forRoot(),
-   //  ModalModule.forRoot()
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgxImageZoomModule
   ],
 
   providers: [],
