81d03be67b207c3868c314277b4e37c49c540cfb
Tuong Nguyen Ba <vansong2204@gmail.com>
2020-10-09 19:46:44 +0700

    them nhieu anh

---
+++ src/assets/js/js/to-sell-product.js
@@ -0,0 +1,17 @@
+function ImagesFileAsURL() {
+  var fileSelected = document.getElementById('upload').files;
+  if (fileSelected.length > 0) {
+      for (var i = 0; i < fileSelected.length; i++) {
+          var fileToLoad = fileSelected[i];
+          var fileReader = new FileReader();
+          fileReader.onload = function(fileLoaderEvent) {
+              var srcData = fileLoaderEvent.target.result;
+              var newImage = document.createElement('img');
+              newImage.src = srcData;
+              document.getElementById('displayImgzzz').innerHTML += newImage.outerHTML;
+          }
+          fileReader.readAsDataURL(fileToLoad);
+      }
+
+  }
+}
