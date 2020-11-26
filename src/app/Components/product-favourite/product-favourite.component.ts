3ff16b23a63b3c814c35424c2896eea36c88b64c
Tuong Nguyen Ba <vansong2204@gmail.com>
2020-11-26 11:58:44 +0700

    edit favorire

--- src/app/Components/product-favourite/product-favourite.component.ts
+++ src/app/Components/product-favourite/product-favourite.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFavouriteComponent implements OnInit {
 
+  p: number;
   constructor() { }
 
   ngOnInit(): void {
   }
+  product = [
+    {
+      image:'https://i.pinimg.com/564x/f3/60/31/f36031f081af4dcfd7f25476c7e9f56b.jpg',
+      name: 'Ghế Sofa GH-8269 ',
+      price: '5.000.000',
+    },
+    {
+      image:'https://i.pinimg.com/564x/f3/60/31/f36031f081af4dcfd7f25476c7e9f56b.jpg',
+      name: 'Bộ Bàn Ghế Cafe',
+      price: '1.000.000',
+    },
+    {
+      image:'https://i.pinimg.com/564x/f3/60/31/f36031f081af4dcfd7f25476c7e9f56b.jpg',
+      name: 'Tủ Lạnh Sanaky 140 Lít',
+      price: '3.000.000',
+    },
+    {
+      image:'https://i.pinimg.com/564x/f3/60/31/f36031f081af4dcfd7f25476c7e9f56b.jpg',
+      name: 'Smart Tivi Led LG 43 inch',
+      price: '4.000.000',
+    }
 
+
+  ]
 }
