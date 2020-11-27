935cf287654cf4f017c5fb94e251c6894d087af6
Tuong Nguyen Ba <vansong2204@gmail.com>
2020-11-27 22:06:08 +0700

    edit to discount-code

--- src/app/Components/discount-code/discount-code.component.ts
+++ src/app/Components/discount-code/discount-code.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-discount-code',
   templateUrl: './discount-code.component.html',
-  styleUrls: ['./discount-code.component.css','./distcount-code.component.scss']
+  styleUrls: ['./discount-code.component.css']
 })
 export class DiscountCodeComponent implements OnInit {
   id: "active"
